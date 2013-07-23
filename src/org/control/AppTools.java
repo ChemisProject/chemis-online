537d24bf76331357dc879f11f2d747c2fbe29d94
guilhermecaldas <guilhermecaldas@yandex.com>
2013-07-23 01:14:02 -0300

    Constants changes- App tools creation

---
+++ src/org/control/AppTools.java
@@ -0,0 +1,34 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.control;
+
+/**
+ *
+ * @author guilherme
+ */
+public class AppTools {
+
+    public static final int B = 1;
+    public static final int KB = 1024;
+    public static final int MB = 1024 * 1024;
+    
+    public static Runtime runtime = Runtime.getRuntime();
+
+    public static long getUsedMemory(int unit) {
+        return (runtime.totalMemory() - runtime.freeMemory()) / unit;
+    }
+
+    public static long getFreeMemory(int unit) {
+        return runtime.freeMemory() / unit;
+    }
+
+    public static long getTotalMemory(int unit) {
+        return runtime.totalMemory() / unit;
+    }
+
+    public static long getMaxMemory(int unit) {
+        return runtime.maxMemory() / unit;
+    }
+}
--- src/org/control/Main.java
+++ src/org/control/Main.java
@@ -4,6 +4,9 @@
  */
 package org.control;
 
+import java.text.DecimalFormat;
+import org.model.ConstantsDixon;
+
 /**
  *
  * @author guilherme
@@ -14,6 +17,14 @@ public class Main {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("Chemis Initialized: Nothing to run yet...");
+        //System.out.println("Chemis Initialized: Nothing to run yet...");
+
+        for (int i = 3; i < 30; i++) {
+            for (int j = 0; j < 1000; j++) {
+                System.out.println("95%: #" + i + "- " + ConstantsDixon.REPETITIONS_CONSTANTS.get("95_" + i));
+                System.out.println("99%: #" + i + "- " + ConstantsDixon.REPETITIONS_CONSTANTS.get("99_" + i));
+            }
+        }
+
     }
 }
