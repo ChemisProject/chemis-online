c557967be95ca109ebe62910e585c2088e53512e
guilhermecaldas <guilhermecaldas@yandex.com>
2014-02-27 21:54:36 -0300

    Modularization

---
+++ Resources/js/dixon-core-ui.js
@@ -0,0 +1,174 @@
+function addValueField() {
+    $("#chemis-dixon-values-list").append(
+        $('<li />').addClass("pure-control-group").append(
+            $('<input />').addClass("chemis-dixon-value").attr("type", "text").attr("placeholder", "0,000")
+        ).append(
+            $("<button />").addClass("pure-button").text("x").attr("onclick", "removeValueField(this)")
+        )
+    );
+    var valuesCount = $(".chemis-dixon-value").length;
+    $("#chemis-dixon-values-count").text("Valores: " + valuesCount)
+}
+
+function removeValueField(self) {
+    var valuesCount = $(".chemis-dixon-value").length;
+    if (valuesCount > 3) {
+        $(self).addClass("chemis-dixon-value-rem");
+        $(self).removeClass("chemis-dixon-value");
+        $(self).parent().fadeOut(300, function () {
+            $(this).remove();
+        });
+        $("#chemis-dixon-values-count").text("Values: " + (valuesCount - 1))
+    }
+}
+
+function getAllFields() {
+    return $("#chemis-dixon-values-list .chemis-dixon-value");
+}
+
+function clearAllFields() {
+    $("#chemis-dixon-values-list").html("");
+    for (i = 0; i < 3; i++) {
+        addValueField();
+    }
+}
+
+function displayResults(dixon, results) {
+    var per = results.percent;
+    if (dixon != null) {
+        $("#result-" + per).html("Aprovado");
+        //$("#le-" + per).html(results.lowerEnd);
+        //$("#ue-" + per).html(results.upperEnd);
+        //$("#n-" + per).html(dixon.getN());
+        //$("#q-" + per).html(dixonConstants[per + "_" + dixon.getN()]);
+        var le = new countUp("le-" + per, 0, results.lowerEnd, 4, 0.7, options);
+        var ue = new countUp("ue-" + per, 0, results.upperEnd, 4, 0.7, options);
+        var n = new countUp("n-" + per, 0, dixon.getN(), 0, 0.7, options);
+        var q = new countUp("q-" + per, 0, dixonConstants[per + "_" + dixon.getN()], 3, 0.7, options);
+        le.start();
+        ue.start();
+        n.start();
+        q.start();
+        if (dixon.removed.length == 0) {
+            $("#rm-" + per).html($("<li />").text("---"));
+        } else {
+            $("#rm-" + per).html("");
+        }
+        $.each(dixon.removed, function () {
+            $("#rm-" + per).append($("<li />").text(this));
+        });
+    } else {
+        $("#result-" + per).html("Reprovado");
+        $("#le-" + per).html("---");
+        $("#ue-" + per).html("---");
+        $("#n-" + per).html("---");
+        $("#q-" + per).html("---");
+        $("#rm-" + per).html($("<li />").text("---"));
+    }
+}
+
+function valuesToDixon() {
+    var values = getAllFields();
+    var dixon = new Dixon();
+    $.each(values, function (index) {
+        var value = $(this).val();
+        value = value.replace(",", ".");
+        value = value.trim();
+        value = parseFloat(value);
+        if (validateField(this, value, index, dixon)) {
+            console.log("Value added:" + value);
+            $(this).removeClass("chemis-dixon-value-invalid");
+            $(this).val(value)
+            dixon.addValue(value);
+        }
+    });
+
+    return dixon;
+}
+
+function validateField(field, value, index, dixon) {
+    if ($(field).val() == "") {
+        removeValueField(field);
+        console.log("Index #" + (index + 1) + " removed - empty value")
+        return false;
+    } else if (dixon.values.indexOf(parseFloat(value)) != -1) {
+        removeValueField(field);
+        console.log("Index #" + (index + 1) + " removed - duplicated value")
+        return false;
+    } else if (Number.isNaN(value)) {
+        $(field).addClass("chemis-dixon-value-invalid");
+        console.log("Index #" + (index + 1) + " alert - Not a Number");
+    } else {
+        return true;
+    }
+}
+
+function calcDixon() {
+    var dixon95 = valuesToDixon();
+    var dixon99 = valuesToDixon();
+
+    try {
+        displayResults(dixon95, DixonControl.calc(dixon95, 95));
+    } catch (err) {
+        console.log(err.message)
+        displayResults(null, JSON.parse('{"percent":95}'));
+    }
+
+    try {
+        displayResults(dixon99, DixonControl.calc(dixon99, 99));
+    } catch (err) {
+        console.log(err.message);
+        displayResults(null, JSON.parse('{"percent":99}'));
+    }
+}
+
+function fieldMoveUp() {
+    if (document.activeElement != null) {
+        var allFields = getAllFields();
+        var index = allFields.index(document.activeElement);
+        if (index != -1 && index != 0) {
+            allFields[index - 1].focus();
+            console.log("Move up")
+        } else if (index == 0) {
+            getAllFields()[0].focus();
+        }
+    }
+}
+
+function fieldMoveDown() {
+    if (document.activeElement != null) {
+        var allFields = getAllFields();
+        var index = allFields.index(document.activeElement);
+        if (index != -1 && index != allFields.length - 1) {
+            allFields[index + 1].focus();
+            console.log("Move down")
+        } else if (index == allFields.length - 1) {
+            addValueField();
+            getAllFields()[index + 1].focus();
+        }
+    }
+}
+
+function removeEmptyField() {
+    var active = document.activeElement;
+    if ($(active).val() == "" && getAllFields().length > 3) {
+        if (getAllFields().index(active) == 0) {
+            fieldMoveDown();
+            removeValueField(active);
+        } else {
+            fieldMoveUp();
+            removeValueField(active);
+        }
+    } else if ($(active).val() == "") {
+        fieldMoveUp();
+    }
+}
+
+var options = {  
+    useEasing: true,
+      useGrouping: true,
+      separator: ',',
+      decimal: '.'
+}
+
+clearAllFields();
